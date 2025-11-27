ca12050f2bf7f22ab7a07115fd9fb419a349b3af
Noob31Gen <noob31.random@proton.me>
2025-11-27 10:58:45 +0530

    version change

--- assets/shortcuts-init-5EjMyyzi.js
+++
@@ -1,1 +0,0 @@
-import{S as i}from"./shortcuts-C-IEUyZ3.js";function t(){i.init(),console.log("Global shortcuts initialized")}export{t as i};
---
+++ assets/shortcuts-init-CkUYOV_A.js
@@ -0,0 +1,1 @@
+import{S as i}from"./shortcuts-CWaHRhee.js";function t(){i.init(),console.log("Global shortcuts initialized")}export{t as i};
